4c17b8b22e5de8b52c0c93933fcbd278d1e6e328
petru <petru.cojocar@gmail.com>
2020-03-02 19:52:22 +0200

    code import optimization (fqn)

--- src/Craft/Http/Controller/Exception/ActionArgumentException.php
+++ src/Craft/Http/Controller/Exception/ActionArgumentException.php
@@ -3,6 +3,7 @@
 namespace Craft\Http\Controller\Exception;
 
 use Craft\Exception\ContextualExceptionTrait;
+use Exception;
 
 /**
  * Class ActionArgumentException
@@ -10,7 +11,7 @@ use Craft\Exception\ContextualExceptionTrait;
  * @author Petru Cojocar <petru.cojocar@gmail.com>
  *
  */
-class ActionArgumentException extends \Exception implements ActionArgumentExceptionInterface
+class ActionArgumentException extends Exception implements ActionArgumentExceptionInterface
 {
     use ContextualExceptionTrait;
 }
